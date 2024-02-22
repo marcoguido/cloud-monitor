b1fc8a550b3f6cc6b705f2037eb6ca3461d7e32c
Marco Guidolin <m.guidolin@egeatech.com>
2024-02-22 09:55:43 +0100

    ADD Filament resource mapping Provider entity

---
+++ app/Filament/Resources/ProviderResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProviderResource\Pages;
+use App\Filament\Resources\ProviderResource\RelationManagers;
+use App\Models\Provider;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ProviderResource extends Resource
+{
+    protected static ?string $model = Provider::class;
+    protected static ?string $recordTitleAttribute = 'name';
+    protected static ?int $navigationSort = 0;
+    protected static ?string $navigationIcon = 'heroicon-o-wifi';
+
+    public static function form(Form $form): Form
+    {
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+            ])
+            ->bulkActions([
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProviders::route('/'),
+            'create' => Pages\CreateProvider::route('/create'),
+            'edit' => Pages\EditProvider::route('/{record}/edit'),
+        ];
+    }
+}
