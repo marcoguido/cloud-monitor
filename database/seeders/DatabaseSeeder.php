f8b50ef2b88424b1e885026237996e4d02c36626
Marco Guidolin <m.guidolin@egeatech.com>
2024-02-22 09:54:30 +0100

    ADD DB seeding for base application environment tags, used by Domain model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Tags\Tag;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Tag::findOrCreate(
+            values: [
+                'Development',
+                'Staging',
+                'QA',
+                'Production',
+            ],
+            type: 'application_environments',
+        );
     }
 }
